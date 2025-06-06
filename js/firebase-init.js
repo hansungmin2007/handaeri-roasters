54801f5c189f8daa0af14cf5739d8e4e08bab173
hansungmin2007 <gkstjdals44@gmail.com>
2025-06-07 08:10:41 +0900

    ㄴㅇㄴㅇ

---
+++ js/firebase-init.js
@@ -0,0 +1,12 @@
+// js/firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDwl47SxBJWtxvJfPGN7B_hueMSwLU1QPE",
+  authDomain: "bean--frame.firebaseapp.com",
+  projectId: "bean--frame",
+  storageBucket: "bean--frame.appspot.com",
+  messagingSenderId: "809663276265",
+  appId: "1:809663276265:web:d215bde62bd9174ea23c78",
+  measurementId: "G-8Y25NWVE0Q"
+};
+
+firebase.initializeApp(firebaseConfig);
